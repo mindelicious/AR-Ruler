f67f65f347548693fc92fac4e6cf7ed395b65d2e
mindelicious <[mailprogramisty@gmail.com]>
2018-12-14 21:07:51 +0100

    Add some functionality (put dot on screen and measure distance)

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -11,16 +11,19 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    var dotNodes = [SCNNode]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
-   
+        
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -28,7 +31,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -39,5 +42,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult =  hitTestResults.first {
+                addDot(at: hitResult)
+            }
+        }
+        
+        
+    }
+    func addDot(at hitResult: ARHitTestResult) {
+        let dotGeometry = SCNSphere(radius: 0.003)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.purple
+        dotGeometry.materials = [material]
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        
+        dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.y)
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            let start = dotNodes[0]
+            let end = dotNodes[1]
+            
+            let distance = sqrt(
+                pow(end.position.x - start.position.x, 2) +
+                pow(end.position.y - start.position.y, 2) +
+                pow(end.position.z - start.position.z, 2)
+            )
+            
+            print(abs(distance))
+        }
+    }
+    
 }
